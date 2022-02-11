59cd24d98cdac847488f171fe27a7d11303b983c
Alexander Higgins <thealexanderhiggins@gmail.com>
2022-02-10 23:56:29 -0500

    init commit

---
+++ chapter-12/src/systems/index.ts
@@ -0,0 +1,8 @@
+export { RenderSystem } from "./render";
+export { VisibilitySystem } from "./visibility";
+export { EnemyAISystem } from "./enemy-ai";
+export { MapIndexing } from "./map-indexing";
+export { MeleeCombat } from "./melee-combat";
+export { DamageSystem } from "./damage-system";
+export { DeathSystem } from "./death-system";
+export { InventorySystem } from "./inventory-system";
